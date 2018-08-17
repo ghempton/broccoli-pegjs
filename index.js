b0ccaf1f8f52a0488349a390cafeaefa136aba9f
Gaurav Munjal <gaurav@peeriq.com>
2018-08-17 12:47:43 -0400

    Update peg.js and use broccoli-persistent-filter

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var Filter = require('broccoli-filter'),
-    PEG    = require('pegjs');
+var Filter = require('broccoli-persistent-filter'),
+    PEG    = require('pegjs'),
+    md5    = require('js-md5');
 
 module.exports = PegFilter;
 PegFilter.prototype = Object.create(Filter.prototype);
@@ -27,6 +28,15 @@ PegFilter.prototype.extensions = ['pegjs'];
 PegFilter.prototype.targetExtension = 'js';
 
 PegFilter.prototype.processString = function (string) {
-  var parser = PEG.buildParser(string, this.options);
+  var parser = PEG.generate(string, this.options);
   return this.options.wrapper(string, parser);
-}
+};
+
+PegFilter.prototype.cacheKey = function () {
+  prototype.cacheKey = function() {
+    const key = md5(Filter.prototype.call(this) +
+      JSON.stringify(this.options) +
+      require('pegjs/package.json').version);
+    return key;
+   }
+};
